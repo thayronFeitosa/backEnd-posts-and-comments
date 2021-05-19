23bfedab5c1b06b9b5b591cabed406ababc3fcf4
thayron <thayronf3@gmail.com>
2021-05-19 02:08:00 -0300

    Foi criado o controller PostCOntroller que faz o post do comentario e lista os posts por paginaçao em ordem desc

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PostRequest;
+use App\Models\Post;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class PostController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function index(): JsonResponse
+    {
+        return response()->json(Post::orderBy('created_at', 'desc')->paginate(5));
+    }
+
+    public function store(PostRequest $request): JsonResponse
+    {
+        $data = $request->all();
+        $data['user_id'] = Auth::user()->id;
+        return response()->json(Post::create($data), 201);
+    }
+}
