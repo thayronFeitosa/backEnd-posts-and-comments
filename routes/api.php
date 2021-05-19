b76d6a026c6419e0825378b751a94cb330479adb
thayron <thayronf3@gmail.com>
2021-05-19 02:02:18 -0300

    Foi implementado a toda de refresh token e de logout da api

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/auth/login', [AuthController::class, 'login']);
+Route::get('/auth/refresh', [AuthController::class, 'refresh']);
+Route::post('/auth/logout', [AuthController::class, 'logout']);
